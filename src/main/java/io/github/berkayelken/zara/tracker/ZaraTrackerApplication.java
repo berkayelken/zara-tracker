4d37306bd4e96122bbc96713d333b22c0a512ace
Berkay Yelken <berkayelken@gmail.com>
2024-11-30 19:25:06 +0300

    init

---
+++ src/main/java/io/github/berkayelken/zara/tracker/ZaraTrackerApplication.java
@@ -0,0 +1,13 @@
+package io.github.berkayelken.zara.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaraTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaraTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/berkayelken/zara/tracker/ZaraTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.berkayelken.zara.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaraTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
